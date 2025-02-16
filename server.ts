26d86a50560dfd702cefe06b7a820ad513307c9c
EpphremM <mandefroephrem48@gmail.com>
2025-02-15 19:58:06 -1000

    setuping socket io fro notification chat and game

--- server.ts
+++ server.ts
@@ -1,14 +1,32 @@
-// import express from 'express'
-import cors from 'cors'
-import http from 'http'
-import {Server}  from  'socket.io'
-import { socketHandler } from './src/socket/socket.handler';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-// const app=express();
-// app.use(cors({origin:"*"}))
-const server=http.createServer();
-const io= new  Server(server);
-socketHandler(io);
-server.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+const app = express();
+const server = createServer(app);
+
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true
+    },
+    transports: ['websocket'], 
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("sendMessage", (message) => {
+        io.emit("sendMessage", message);  
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
